091838a4c67afe19e39a4b2997d8ae61ddc57cd8
marisaadati <saadati.marzieh92@gmail.com>
2022-07-09 10:33:52 +0430

    add project

--- src/Components/Weblayout/Toolbar.js
+++ src/Components/Weblayout/Toolbar.js
@@ -4,8 +4,52 @@ import "./Toolbar.css";
 const Toolbar = () => {
   return (
     <div>
-      <div className=" bg-rose-700 w-screen h-11 container">
-        <ul className="flex text-white text-sm space-x-4 ml-24 pt-3">
+      <nav class="flex items-center justify-between flex-wrap bg-rose-700 h-11  p-6">
+        <div class="block lg:hidden">
+          <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg
+              class="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div class="text-sm lg:flex-grow">
+            <a
+              href="#responsive-header"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              Docs
+            </a>
+            <a
+              href="#responsive-header"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            >
+              Examples
+            </a>
+            <a
+              href="#responsive-header"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            >
+              Blog
+            </a>
+          </div>
+          <div>
+            <a
+              href="#"
+              class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            >
+              Download
+            </a>
+          </div>
+        </div>
+      </nav>
+      {/* <div className=" flex grid-rows-3 bg-rose-700 w-screen h-11 container">
+        <ul className=" text-white text-sm space-x-4 ml-24 pt-3">
           <li>
             {" "}
             <a href="/global/en/New-Arrivals/cms/new-arrivals">New Arrivals</a>
@@ -27,7 +71,7 @@ const Toolbar = () => {
             />
           </li>
         </ul>
-      </div>
+      </div> */}
     </div>
   );
 };
