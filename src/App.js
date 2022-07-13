f2f4256d6395f69409f2bed479520898ca05d387
fatemehkhorshidvand <fatemehkhorshd@gmail.com>
2022-07-13 03:23:00 -0700

    image grid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import "./App.css";
-import Nife from "./image/download.web1.webp";
-import newe from "./image/download.3.jpg";
-import flow from "./image/download.webp";
-import image from "./image/download.3.webp";
-import watch from "./image/download.4.webp";
+import victorinox from "./image/download.web1.webp";
+import girl from "./image/download.3.jpg";
+import nife from "./image/download.webp";
+import goje from "./image/download.3.webp";
+import watch from "./image/download.watch.webp";
 function App() {
   return (
     <div>
-      <div className=" grid  grid-cols-1 ">
-        <img
-          className="col-start-1 col-end-4 row-start-1 row-end-3  m-auto"
-          src={Nife}
-          alt=""
-        />
-        <img className="col-start-1 col-start: 1; " src={newe} alt="" />
-        <img className="" src={flow} alt="" />
-        <img className="" src={image} alt="" />
-        <img className="" src={watch} alt="" />
+      <div className=" px-[130px] grid grid-cols-3 gap-3  ">
+        <div className="w-full h-fit col-span-full">
+          <img className="w-full h-full " src={victorinox} alt="" />
+        </div>
+        <div className=" col-span-2">
+          <div className="w-full  ">
+            <img className="w-full   " src={girl} alt="" />
+          </div>
+          <div className="w-full mt-[10px] relative ">
+            <img className="w-full" src={goje} alt="" />
+            <h1 className="absolute bottom-0 bg-red-500">dfhdcdhdcjhsdcjhsdcsd</h1>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="w-full relative">
+            <img className="w-full  " src={nife} alt="" />
+          <h1 className="absolute bottom-0 bg-red-500">dfhdcdhdcjhsdcjhsdcsd</h1>
+          </div>
+
+          <div className="w-full  mt-[10px] ">
+            <img className="w-full " src={watch} alt="" />
+          </div>
+          <div className="w-full relative  mt-[10px] ">
+            <img className="w-full " src={watch} alt="" />
+          </div>
+        </div>
       </div>
+      {/* <div className="flex justify-between px-16">
+        <div>l</div>
+        <div>r</div>
+      </div> */}
     </div>
   );
 }
