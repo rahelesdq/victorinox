a5c7aa2581e3b27770bfb5ad42ae9430120925db
rahelesdq <raheleh.sediq@gmail.com>
2022-07-16 17:02:54 +0430

    ss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import "./App.css";
 import Imagegrid from "./Components/Homepage/Imagegrid";
-<<<<<<< HEAD
 import ImageSlider from "./Components/home image slider/imageSlider";
-=======
 import Hello from "./Components/Homepage/Hello";
->>>>>>> ab5a4c2f285baee862b44d5075a4f5d96ed27732
 
 function App() {
 
@@ -12,12 +9,9 @@ function App() {
 
     <div>
       <Imagegrid />
-<<<<<<< HEAD
       <ImageSlider />
 
-=======
       <Hello />
->>>>>>> ab5a4c2f285baee862b44d5075a4f5d96ed27732
     </div>
   );
 }
