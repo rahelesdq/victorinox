58d931d65e24d1a2c69c2eba9d9cbe2735d34d57
rahelesdq <raheleh.sediq@gmail.com>
2022-07-20 14:10:26 +0430

    image slidere

--- src/Components/Watches/Watches.js
+++ src/Components/Watches/Watches.js
@@ -1,20 +1,26 @@
-import React from "react"
+import React, { Component } from "react";
 
-
-
-
-
-const Watches =()=>{
-
-
-return(
-    <div>
-
-
-    </div>
-)
-
-
+class Watches extends Component {
+  render() {
+    return (
+      <div>
+        <a className="top-16" href="/">
+          home
+        </a>
+        <div className="victorinox-text xl:ml-20 xl:mr-96 right-96 flex">
+          <h1>Victorinox Watches</h1>
+          <span className="xl:text-left ">
+            ​​​​​​​125 years ago, Karl Elsener didn’t just invent the Swiss Army
+            Knife – he invented an attitude. To this day, this attitude inspires
+            every single one of our products – including our watches. Our
+            timepieces are made right here in Switzerland and will inspire you
+            to realize that time having fun is never wasted.
+            
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Watches;
--- src/Components/home image slider/imageSlider.js
+++ src/Components/home image slider/imageSlider.js
@@ -1,74 +1,72 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
-import Carosel from "carousel-react-rcdev";
+import Cards from "./cards";
 
 class ImageSlider extends Component {
   render() {
+    
     return (
       <div>
-        <Carosel>
-          <img
-            className="border-2 "
-            src="https://imageengine.victorinox.com/mediahub/134519/420Wx368H/TGE_611414_PO.jpg"
-            alt="imagem"
-            title="imagem"
-            
-          ></img>
-
-          <img
-            className="border-2 "
-            src="https://imageengine.victorinox.com/mediahub/31254/420Wx368H/CUT_6_7143_5__S1.jpg"
-            alt="imagem"
-            title="imagem"
-          ></img>
-
-          <img
-            className="border-2 "
-            src="https://imageengine.victorinox.com/mediahub/31912/420Wx368H/CUT_7_7400_20G__S1.jpg"
-            alt="imagem"
-            title="imagem"
-          ></img>
-
-          <img
-            className="border-2 "
-            src="https://imageengine.victorinox.com/mediahub/177943/420Wx368H/TGE_612120_S_PO.jpg"
-            alt="imagem"
-            title="imagem"
-          ></img>
-
-          <img
-            className="border-2 "
-            src="https://imageengine.victorinox.com/mediahub/150332/420Wx368H/WAT_241904_S1.jpg"
-            alt="imagem"
-            title="imagem"
-          ></img>
-
-          <img
-            className="border-2 "
-            src="https://imageengine.victorinox.com/mediahub/32624/420Wx368H/SAK_0_6203__S1.jpg"
-            alt="imagem"
-            title="imagem"
-          ></img>
-
-          <img
-            className="border-2 "
-            src="https://imageengine.victorinox.com/mediahub/150332/420Wx368H/WAT_241904_S1.jpg"
-            alt="imagem" 
-            title="imagem"
-          ></img>
-        </Carosel>
+          <p className="text-center xl:text-4xl font-semibold xl:m-12 xl:p-5 md:text-5xl">Our Best Sellers</p>
+          <Cards />
       </div>
     );
   }
 }
 
 export default ImageSlider;
-=======
-import React from "react";
 
-const imageSlider = () => {
-  return <div></div>;
-};
 
-export default imageSlider;
->>>>>>> ab5a4c2f285baee862b44d5075a4f5d96ed27732
+
+
+
+// <Carosel>
+// <img
+//   className="border-2 "
+//   src="https://imageengine.victorinox.com/mediahub/134519/420Wx368H/TGE_611414_PO.jpg"
+//   alt="imagem"
+//   title="imagem"
+
+// ></img>
+
+// <img
+//   className="border-2 "
+//   src="https://imageengine.victorinox.com/mediahub/31254/420Wx368H/CUT_6_7143_5__S1.jpg"
+//   alt="imagem"
+//   title="imagem"
+// ></img>
+
+// <img
+//   className="border-2 "
+//   src="https://imageengine.victorinox.com/mediahub/31912/420Wx368H/CUT_7_7400_20G__S1.jpg"
+//   alt="imagem"
+//   title="imagem"
+// ></img>
+
+// <img
+//   className="border-2 "
+//   src="https://imageengine.victorinox.com/mediahub/177943/420Wx368H/TGE_612120_S_PO.jpg"
+//   alt="imagem"
+//   title="imagem"
+// ></img>
+
+// <img
+//   className="border-2 "
+//   src="https://imageengine.victorinox.com/mediahub/150332/420Wx368H/WAT_241904_S1.jpg"
+//   alt="imagem"
+//   title="imagem"
+// ></img>
+
+// <img
+//   className="border-2 "
+//   src="https://imageengine.victorinox.com/mediahub/32624/420Wx368H/SAK_0_6203__S1.jpg"
+//   alt="imagem"
+//   title="imagem"
+// ></img>
+
+// <img
+//   className="border-2 "
+//   src="https://imageengine.victorinox.com/mediahub/150332/420Wx368H/WAT_241904_S1.jpg"
+//   alt="imagem"
+//   title="imagem"
+// ></img>
+// </Carosel>
