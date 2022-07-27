eef57ec6d312ab29b35291efec52a6c04db43687
marisaadati <saadati.marzieh92@gmail.com>
2022-07-27 10:50:31 +0430

    product

--- src/Components/Dashboard/Myproducts.js
+++ src/Components/Dashboard/Myproducts.js
@@ -1,20 +1,21 @@
-import React from "react"
+import React from "react";
+import SidebarDashboard from "./SidebarDashboard";
 
-
-
-
-
-const Myproducts =()=>{
-
-
-return(
+const Myproducts = () => {
+  return (
     <div>
-
-
+      <section className="container flex justify-between">
+        <div className="flex">
+          <SidebarDashboard />
+        </div>
+        <div className="flex border-b border-black w-56  ">
+          <button className=" bg-rose-700 h-12 w-52 mt-16 mr-10 text-white">
+            ADD NEW PRODUCT
+          </button>
+        </div>
+      </section>
     </div>
-)
-
-
-}
+  );
+};
 
 export default Myproducts;
