8c3916ff01d4e0e18b2032043ed87f2259b83065
rahelesdq <raheleh.sediq@gmail.com>
2022-07-27 10:47:15 +0430

    profile

--- src/Components/Dashboard/Myprofile.js
+++ src/Components/Dashboard/Myprofile.js
@@ -4,31 +4,31 @@ import SidebarDashboard from "./SidebarDashboard";
 
 const Myprofile = () => {
   return (
-    <div className="flex">
+    <div className="flex ">
       <SidebarDashboard />
-      <div className="mt-[100px]    ">
-        <span className="text-3xl ml-[290px] ">My Account</span>
-        <div className="ml-[290px] text-2xl mt-[50px]  ">
-          <span>title*</span>
-          <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" class="w-7 xl:ml-[400px] text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+      <div className="mt-[100px]  mb-[300px]  mr-[50px]  ">
+        <span className="text-3xl  ">My Account</span>
+        <div className="flex  text-2xl mt-[100px]   ">
+          <span className="">title*</span>
+          <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" class="   w-7 xl:ml-[400px] text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
           <labe>Mr</labe>
-          <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" class="w-7 ml-[200px] text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
+          <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" class="   w-7 xl:ml-[450px] text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
           <labe>Ms./Mrs.</labe>
         </div>
-        <div className="flex ml-[290px] text-2xl mt-[50px]">
-          <span>First Name*</span>
-          <input class="sm:w-full border-gray-500 border-2 ml-[325px] pl-2 h-[48px]	 	"   type="text" ></input>
+        <div className="flex  text-2xl mt-[50px]">
+          <span>FirstName*</span>
+          <input class="sm:w-full border-gray-400 border-2 ml-[325px] pl-2 h-[40px]	"   type="text" ></input>
         </div>
-        <div className="flex ml-[290px] text-2xl mt-[50px]">
+        <div className="flex  text-2xl mt-[50px]">
         <span>Surname*</span>
-          <input class="sm:w-full border-gray-500 border-2 ml-[295px] pl-2 h-[48px]	"  type="text" ></input>
+          <input class="sm:w-full border-gray-400 border-2 ml-[295px] pl-2 h-[40px]	"  type="text" ></input>
         
         </div>
-        <div className="flex ml-[290px] text-2xl mt-[50px]">
+        <div className="flex  text-2xl mt-[50px]">
         <span>Nickname*</span>
-          <input class="sm:w-full border-gray-500 border-2 ml-[287px] pl-2 h-[48px]		"  type="text" ></input>
+          <input class="sm:w-full border-gray-400 border-2 ml-[287px] pl-2 h-[40px]		"  type="text" ></input>
         </div>
-        <div className="flex ml-[290px] text-2xl mt-[50px]">
+        <div className="flex  text-2xl mt-[50px]">
           <span>Country/Region*</span>
           <select id="country" name="country" autocomplete="country" class="ml-[277px]  w-[253px]  px-3  bg-white border-b-2 border-black  shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                   <option>Iran</option>
@@ -37,7 +37,7 @@ const Myprofile = () => {
                   <option>Mexico</option>
                 </select>
         </div>
-        <div className="flex ml-[290px] text-2xl mt-[50px]">
+        <div className="flex  text-2xl mt-[50px]">
           <span>Language*</span>
           <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" class="w-7 ml-[200px] text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
           <labe>English</labe>
@@ -48,9 +48,9 @@ const Myprofile = () => {
           <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" class="w-7 ml-[20px] text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"></input>
           <labe>Spanish</labe>
         </div>
-        <div className="flex ml-[290px] text-2xl mt-[50px]">
+        <div className="flex  text-2xl mt-[50px]">
           <span>Birthday</span>
-          <select id="month" name="month"  class="ml-[127px]  w-[253px]  px-3  bg-white border-b-2 border-black  shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+          <select id="month" name="month"  class="ml-[127px]  w-[100px]  px-3  bg-white border-b-2 border-black  shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                   <option>1</option>
                   <option>2</option>
                   <option>3</option>
@@ -64,7 +64,7 @@ const Myprofile = () => {
                   <option>11</option>
                   <option>12</option>
                 </select>
-           <select id="day" name="day"  class=" w-[253px]  px-3  bg-white border-b-2 border-black  shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+           <select id="day" name="day"  class=" w-[100px]  px-3  bg-white border-b-2 border-black  shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
            <option>1</option>
                   <option>2</option>
                   <option>3</option>
@@ -97,7 +97,7 @@ const Myprofile = () => {
                   <option>30</option>
                   <option>31</option>
                 </select>
-           <select id="year" name="year"  class="  w-[253px]  px-3  bg-white border-b-2 border-black  shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+           <select id="year" name="year"  class="  w-[150px]  px-3  bg-white border-b-2 border-black  shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                   <option>1980-1990</option>
                   <option>1990-2000</option>
                   <option>2000-2010</option>
@@ -105,7 +105,7 @@ const Myprofile = () => {
                 </select>     
 
         </div>
-        <div className="flex ml-[290px] text-2xl mt-[50px]">
+        <div className="flex  text-2xl mt-[50px]">
 
         <button className="border-2 border-black w-[230px] h-[50px]" >Change</button>
         </div>
